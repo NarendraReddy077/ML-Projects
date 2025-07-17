21e56a203e2a3737d23449f596e956ffbc7b9c4a
NarendraReddy077 <narendrareddymolakala@gmail.com>
2025-07-17 08:17:55 +0530

    Rounded R2 Score

--- src/utils.py
+++ src/utils.py
@@ -30,7 +30,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            test_model_score = round(r2_score(y_test, y_test_pred), 3)
             model_report[model_name] = test_model_score
         
         return model_report
