acab984397faf92ea6733ac4150235bf983a0109
NarendraReddy077 <narendrareddymolakala@gmail.com>
2025-07-18 15:10:29 +0530

    created app

--- src/utils.py
+++ src/utils.py
@@ -40,10 +40,17 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            train_model_score = r2_score(y_train, y_train_pred)
+            # train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = round(r2_score(y_test, y_test_pred), 3)
             model_report[model_name] = test_model_score
         
         return model_report
     except Exception as e:
         CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
